83988f1042dec950d0115fe9b749fc096dee11d5
w3bOs <75303094+w3bOs@users.noreply.github.com>
2020-12-13 01:00:38 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -2,7 +2,7 @@
 // @name         DBFG
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  ssj4() ssj5() lssj() mui() ssjr()
+// @description  ssj4() ssj5() lssj() mui() ssjr() kaio() characters()
 // @author       CiberWeaboo
 // @match        https://japeal.com/dbfg/
 // @grant        none
@@ -10,6 +10,6 @@
 
 const script= document.createElement("script")
 script.type = "text/javascript"
-script.src = "https://hastebin.com/raw/yazexunowo"
+script.src = "https://hastebin.com/raw/docokobuxu"
 document.head.appendChild(script)
 eval(script)
