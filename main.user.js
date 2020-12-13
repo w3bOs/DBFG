2cded7505885cfe7a1675b0aba31e6ea0c35f38b
w3bOs <75303094+w3bOs@users.noreply.github.com>
2020-12-13 02:35:10 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         DBFG
+// @name         Japeal ToolKit
 // @namespace    https://github.com/w3bOs/DBFG/raw/main/main.user.js
-// @version      1.1
-// @description  ssj4() ssj5() lssj() mui() ssjr() kaio() characters()
+// @version      1.3
+// @description  ssj4() ssj5() lssj() mui() ssjr() kaio() characters() megaevo()
 // @author       CiberWeaboo
 // @match        https://japeal.com/dbfg/
 // @grant        none
@@ -10,6 +10,6 @@
 
 const script= document.createElement("script")
 script.type = "text/javascript"
-script.src = "https://hastebin.com/raw/docokobuxu"
+script.src = "https://hastebin.com/raw/gitofexusa"
 document.head.appendChild(script)
 eval(script)
