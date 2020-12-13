812dbe7c5973968c0342908ad4486ca1c31d61a8
w3bOs <75303094+w3bOs@users.noreply.github.com>
2020-12-13 06:58:16 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Japeal ToolKit
 // @namespace    https://github.com/w3bOs/DBFG/raw/main/main.user.js
-// @version      1.4
-// @description  ssj4() ssj5() lssj() mui() ssjr() kaio() characters() megaevo() shiny()
+// @version      1.5
+// @description  ssj4() ssj5() lssj() mui() ssjr() kaio() characters() megaevo() shiny() ssj() ssj2() ssj3() ssjg() ssjb() ssjbe() ui() c() sschar()
 // @author       CiberWeaboo
 // @include      https://japeal.com/dbfg/
 // @include      https://japeal.com/pkm/
@@ -11,6 +11,6 @@
 
 const script= document.createElement("script")
 script.type = "text/javascript"
-script.src = "https://hastebin.com/raw/aruhulobov"
+script.src = "https://hastebin.com/raw/nowinabiku"
 document.head.appendChild(script)
 eval(script)
