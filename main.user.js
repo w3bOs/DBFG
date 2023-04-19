2ffb38177a00f08fc42e0128b251d6eacb77a308
w3bOs <75303094+w3bOs@users.noreply.github.com>
2023-04-19 13:48:37 +0200

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Japeal ToolKit
 // @namespace    https://github.com/w3bOs/DBFG/raw/main/main.user.js
-// @version      4.0
+// @version      5.0
 // @description  Dragon Ball Fusion Generator Toolbox
 // @author       CiberWeaboo
 // @include      https://japeal.com/dbfg/
@@ -24,3 +24,4 @@ script.innerHTML = code
 document.head.appendChild(script)
 eval(script)
 document.getElementsByClassName("ht-site-title")[0].innerHTML = "<a href=\"https://github.com/w3bOs/DBFG\" rel=\"home\">Japeal Toolbox</a>"
+document.getElementById("ShareBar").innerHTML = "<div style=\"position: absolute;width: 300px;text-align: left;top: 5px;\"><font color=\"white\">Command: </font></div>\n\t<div style=\"position: absolute;left: 130;\"><input style=\"width: 254px;\" id=\"permalink\" placeholder=\"Command here :)\" value=\"https://dbfg.japeal.com/\" onclick=\"this.focus(); \" type=\"text\" size=\"30px/\">\n  <input type=\"button\" onclick=\"eval(document.getElementById('permalink').value)\" id=\"visible-button\" value=\"Execute\" style=\"z-index: 900; position: absolute; left: 260px; top: 2px; font-size: 14px; height: 36px; display: block;\"></div>"
