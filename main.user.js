86a731aa073c335e28868de3f9eef86ecb0e6cc8
w3bOs <75303094+w3bOs@users.noreply.github.com>
2023-10-21 08:30:47 +0200

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,27 +1,64 @@
 // ==UserScript==
-// @name         Japeal ToolKit
-// @namespace    https://github.com/w3bOs/DBFG/raw/main/main.user.js
-// @version      5.0
+// @name         Japeal Toolbox v2
+// @namespace    http://tampermonkey.net/
+// @version      1.0
 // @description  Dragon Ball Fusion Generator Toolbox
-// @author       CiberWeaboo
-// @include      https://japeal.com/dbfg/
-// @include      https://japeal.com/pkm/
+// @author       Ciber
+// @match        https://japeal.com/dbfg/
 // @grant        none
 // ==/UserScript==
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+const toolTitle = "Japeal Toolbox Enabled";
+const toolUrl = "https://github.com/w3bOs/DBFG";
+
+function JPv2() {
+
+    // Title Change
+    document.getElementsByClassName("ht-site-title")[0].firstChild.innerHTML = toolTitle;
+    document.getElementsByClassName("ht-site-title")[0].firstChild.href = toolUrl;
+
+    // VIP Activation :P
+    window.VIPstatus = "Y";
+
+    // Forms Patches
+    document.querySelector("#kbtn").setAttribute("onclick", "KaiokenOn()");
+    document.querySelector("#kbtn").setAttribute("src", "Kaioken01.png");
+
+    document.querySelector("#PUButtonsSS4").setAttribute("onclick", "MasterSSCheckPre(-3, -3, 5)");
+    document.querySelector("#PUButtonsSS4").setAttribute("src", "PUButtonsSS4.png");
+
+    document.querySelector("#PUButtonsSS5").setAttribute("onclick", "MasterSSCheckPre(-3, -3, 11)");
+    document.querySelector("#PUButtonsSS5").setAttribute("src", "PUButtonsSS5.png");
+
+    document.querySelector("#PUButtonsLSS").setAttribute("onclick", "MasterSSCheckPre(-3, -3, 12)");
+    document.querySelector("#PUButtonsLSS").setAttribute("src", "PUButtonsLSS.png");
+
+    document.querySelector("#PUButtonsUE").setAttribute("onclick", "MasterSSCheckPre(-3, -3, 13)");
+    document.querySelector("#PUButtonsUE").setAttribute("src", "PUButtonsUE.png");
+
+	// Background Patches
+	var bg1Elements = [19, 21, 23, 24, 25, 26, 29, 30, 31];
+
+	bg1Elements.forEach(function(n) {
+		document.getElementById(`idBG${n}`).setAttribute("onclick", `changeBG${n}();`);
+		document.getElementById(`idBG${n}`).setAttribute("src", `Backgrounds/sprBG0_${n}.jpg`);
+	});
+
+	// Background Patches 2
+	var lockedBGs = Array.from(document.getElementsByClassName("lockedBGClass"));
+
+	lockedBGs.forEach(function(bgElement) {
+		bgElement.remove();
+	});
+
+	var bg2Elements = [5, 7, 8, 9, 10, 13, 14, 16, 17, 18, 19, 20, 21, 22, 25, 26, 29, 32];
+
+	bg2Elements.forEach(function(n) {
+		document.querySelector(`#idArtBG${n}`).setAttribute("onclick", `changeBG(${n});`);
+		document.querySelector(`#idArtBG${n}`).parentElement.setAttribute("onclick", "");
+	});
+
 }
 
-var code = httpGet("https://raw.githubusercontent.com/w3bOs/DBFG/main/workflow")
-const script= document.createElement("script")
-script.type = "text/javascript"
-script.innerHTML = code
-document.head.appendChild(script)
-eval(script)
-document.getElementsByClassName("ht-site-title")[0].innerHTML = "<a href=\"https://github.com/w3bOs/DBFG\" rel=\"home\">Japeal Toolbox</a>"
-document.getElementById("ShareBar").innerHTML = "<div style=\"position: absolute;width: 300px;text-align: left;top: 5px;\"><font color=\"white\">Command: </font></div>\n\t<div style=\"position: absolute;left: 130;\"><input style=\"width: 254px;\" id=\"permalink\" placeholder=\"Command here :)\" value=\"https://dbfg.japeal.com/\" onclick=\"this.focus(); \" type=\"text\" size=\"30px/\">\n  <input type=\"button\" onclick=\"eval(document.getElementById('permalink').value)\" id=\"visible-button\" value=\"Execute\" style=\"z-index: 900; position: absolute; left: 260px; top: 2px; font-size: 14px; height: 36px; display: block;\"></div>"
+// Execute the tool every 1 second (1000 miliseconds)
+setTimeout(JPv2, 1000);
